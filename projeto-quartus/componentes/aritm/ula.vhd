a8abc7822d8205c691480b855f5773f99f19ce0e
Juan Carlos <juanksoliveira@gmail.com>
2018-04-19 01:26:21 -0300

    Finalização da ULA.

--- projeto-quartus/componentes/aritm/ula.vhd
+++ projeto-quartus/componentes/aritm/ula.vhd
@@ -21,5 +21,6 @@ BEGIN
 		std_logic_vector(to_unsigned(to_integer(unsigned(A)) - to_integer(unsigned(B)), 8))	WHEN "001", -- SUBTRACAO	 
 		(A AND B)	WHEN "010", -- AND
 		(A XOR B)	WHEN "011", -- XOR
+		NOT(B)	WHEN "100", -- NOT
 		(OTHERS => '0') WHEN OTHERS;
 END comportamento;
